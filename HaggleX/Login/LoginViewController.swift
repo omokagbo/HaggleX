3757db3e2151576b6863059a4273028d8f87a41d
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-10-20 14:41:22 +0100

    create design for login screen

---
+++ HaggleX/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  HaggleX
+//
+//  Created by Emmanuel Omokagbo on 20/10/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+}
