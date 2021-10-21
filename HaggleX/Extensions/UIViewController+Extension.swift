6b567b43e8c554d6c475c30e386db2314d6f72f3
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-10-21 01:24:17 +0100

    create methods and outlets for verify email screen

--- HaggleX/Extensions/UIViewController+Extension.swift
+++ HaggleX/Extensions/UIViewController+Extension.swift
@@ -11,12 +11,15 @@ extension UIViewController {
     
     public func setupNavbar() {
         navigationController?.navigationBar.tintColor = .white
+        
         self.navigationController?.navigationBar.topItem?.title = " "
     }
     
     public func showDefaultAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
         self.present(alert, animated: true)
     }
     
@@ -24,6 +27,7 @@ extension UIViewController {
         guard let createAccountVC = UIStoryboard(name: "CreateAccount", bundle: nil).instantiateViewController(withIdentifier: "CreateAccountViewController") as? CreateAccountViewController else {
             return
         }
+        
         navigationController?.pushViewController(createAccountVC, animated: true)
     }
     
@@ -31,15 +35,29 @@ extension UIViewController {
         guard let verifyAccountVC = UIStoryboard(name: "VerifyAccount", bundle: nil).instantiateViewController(withIdentifier: "VerifyAccountViewController") as? VerifyAccountViewController else {
             return
         }
+        
         navigationController?.pushViewController(verifyAccountVC, animated: true)
+    }
+    
+    public func presentSetupCompleteScreen() {
+        guard let setupCompleteVC = UIStoryboard(name: "SetupComplete", bundle: nil).instantiateViewController(withIdentifier: "SetupCompleteViewController") as? SetupCompleteViewController else {
+            return
+        }
+        
+        setupCompleteVC.modalPresentationStyle = .fullScreen
+        
+        navigationController?.present(setupCompleteVC, animated: true)
     }
     
     public func presentDashboard() {
         guard let dashboardViewController = UIStoryboard(name: "Dashboard", bundle: nil).instantiateViewController(withIdentifier: "DashboardViewController") as? DashboardViewController else {
             return
         }
+        
         dashboardViewController.modalPresentationStyle = .fullScreen
+        
         dashboardViewController.modalTransitionStyle = .flipHorizontal
+        
         self.navigationController?.present(dashboardViewController, animated: true)
     }
     
--- HaggleX/Verify Account/VerifyAccountViewController.swift
+++ HaggleX/Verify Account/VerifyAccountViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class VerifyAccountViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var verificationCodeTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavbar()
     }
     
+    // MARK: - Actions
     
+    @IBAction func didTapVerifyMe(_ sender: UIButton) {
+        // verify if email is correct
+        
+        // present setup complete screen
+        self.presentSetupCompleteScreen()
+    }
+    
+    @IBAction func didTapResendCode(_ sender: UIButton) {
+        self.showDefaultAlert(title: "Ooops", message: "Functionality coming soon")
+    }
+    
+    // MARK: - Methods
+    
+    private func verifyEmail() {
+        
+    }
 
 }
