beae37e4c311d0e51e44a57bcf022047c09e7e67
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-10-21 02:19:36 +0100

    create market place view

--- HaggleX/Dashboard/DashboardViewController.swift
+++ HaggleX/Dashboard/DashboardViewController.swift
@@ -8,9 +8,49 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var balanceLabel: UILabel!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    // MARK: - Actions
+    
+    @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
+    }
+    
 }
+
+// MARK: - CollectionView Extensions
+
+extension DashboardViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
+
+extension DashboardViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+extension DashboardViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width - 40), height: 100)
+    }
+}
+
+
